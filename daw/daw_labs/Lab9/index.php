eb1299841fb51be7590337ff2427b621c6d03815
rcgc <rcgc140298@gmail.com>
2019-02-26 23:00:46 -0600

    subir avance preeliminar lab9

--- daw/daw_labs/Lab9/index.php
+++ daw/daw_labs/Lab9/index.php
@@ -1,42 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Compiled and minified CSS -->
-  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  	
-	<title></title>
-</head>
-<body>
-	<main>
-		<?php require_once("util.php"); ?>
-		<?php encabezado(); ?>
-		<?php imprimir_titulo("<div class='container'><h1 class='center-align'>Laboratorio 9</h1></div><br>"); ?>	
-		<?php set_tab_lab_number(9); ?>
+<?php 
 
-		<?php echo "<div class='container'><p>Una función que reciba un arreglo de números y devuelva su promedio</p></div><br>";
+	require_once("util.php");
+	
+	encabezado();
+	imprimir_titulo("<div class='container'><h1 class='center-align'>Laboratorio 9</h1></div>");
 
-			echo "<div class='container'><p>Una función que reciba un arreglo de números y devuelva su mediana</p></div><br>";
+	set_tab_lab_number(9);
 
-			echo "<div class='container'><p>Una función que reciba un arreglo de números y muestre la lista de números, y como ítems de una lista html muestre el promedio, la media, y el arreglo ordenado de menor a mayor, y posteriormente de mayor a menor</p></div><br>";	
+	imprimir_subtitulo("<div class='container'><h3 class='left-align'>Ejercicios</h3></div><br>");
 
-			echo "<div class='container'><p>Una función que imprima una tabla html, que muestre los cuadrados y cubos de 1 hasta un número n</p></div><br>";
+	exercise1();
 
-			echo "<div class='container'><p>Escoge algún problema que hayas implementado en otro lenguaje de programación, y dale una solución en php, en una vista agradable para el usuario.</p></div><br>";	?>
+	exercise2();
 
-			
-		<?php imprimir_subtitulo("<br><div class='container'><h3 class='left-align'>Preguntas</h3></div><br>"); ?>
-		<?php questions();?>
-	</main>
-	<?php //phpinfo(); ?>
-	<?php footer();	?>
-	<!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-	<!--Auto iniciar atributos Materialize-->
-	<script type="text/javascript">
+	exercise3();
 
-		M.AutoInit();
-		
-	</script>
-</body>
-</html>
+	exercise4();
+
+	exercise5();
+	
+	imprimir_subtitulo("<br><div class='container'><h3 class='left-align'>Preguntas</h3></div><br>");
+
+	questions();
+
+	//phpinfo();
+
+	footer();	
+
+?>
--- daw/daw_labs/Lab9/util.php
+++ daw/daw_labs/Lab9/util.php
@@ -1,4 +1,5 @@
 <?php
+
 	 function encabezado() {
         include("includes/_navbar.html");
     }
@@ -22,4 +23,190 @@
     function set_tab_lab_number($num){
     	echo "<title>Lab".$num."</title>";
     }
-?>
+
+    function exercise1(){
+        include("includes/_ex1.html");   
+
+        if(isset($_POST['btn1'])){
+                $value = $_POST['txt1'];
+                
+                print_array_string($value);
+
+                promedio($value);
+        }
+    }
+
+    function print_array_string($value){
+        $numbers = explode(" ", $value);
+
+        $arrlength = count($numbers);
+
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+        echo "<strong>Array original: </strong>";
+        for($x = 0; $x < $arrlength; $x++) {
+            echo $numbers[$x];
+            echo " ";
+        }
+        
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";
+    }
+
+    function print_array_numbers_asc($numbers){
+        
+        sort($numbers);
+
+        $arrlength = count($numbers);
+
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+        echo "<strong>Array ascendente: </strong>";
+        for($x = 0; $x < $arrlength; $x++) {
+            echo $numbers[$x];
+            echo " ";
+        }
+        
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";
+    }
+
+    function print_array_numbers_desc($numbers){
+        
+        rsort($numbers);
+
+        $arrlength = count($numbers);
+
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+        echo "<strong>Array descendente: </strong>";
+        for($x = 0; $x < $arrlength; $x++) {
+            echo $numbers[$x];
+            echo " ";
+        }
+        
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";
+    }
+
+    function promedio($value){
+        $numbers = explode(" ", $value);
+                
+        $arrlength = count($numbers);
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+
+        echo "<strong>Promedio: </strong>".array_sum($numbers)/$arrlength;
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";
+    }
+
+    function exercise2(){
+        include("includes/_ex2.html"); 
+
+        if(isset($_POST['btn2'])){
+                $value = $_POST['txt2'];
+
+                print_array_string($value);
+
+                $numbers = explode(" ", $value);
+                sort($numbers);
+
+                print_array_numbers_asc($numbers);
+
+                mediana($value);
+        }  
+    }
+
+    function mediana($value){
+        $numbers = explode(" ", $value);
+        sort($numbers);
+                
+        $arrlength = count($numbers);
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+        
+        if($arrlength %2 ==0){
+            echo "<strong>Mediana: </strong>".($numbers[($arrlength/2)-1]+$numbers[($arrlength/2)])/2;
+        }else{
+            echo "<strong>Mediana: </strong>".($numbers[ceil(($arrlength/2)-1)]);
+        }
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";   
+    }
+
+    function exercise3(){
+        include("includes/_ex3.html");
+        if(isset($_POST['btn3'])){
+                $value = $_POST['txt3'];
+                problema3($value);
+        }
+    }
+
+    function problema3($value){
+        $numbers = explode(" ", $value);
+        $arrlength = count($numbers);
+        echo "<div class= 'container'>";
+        echo "<div class= 'center-align'>";
+
+        print_array_string($value);
+
+        promedio($value);
+
+        mediana($value);
+
+        print_array_numbers_asc($numbers);
+        print_array_numbers_desc($numbers);
+
+        echo "</div>";
+        echo "</div>";
+        echo "<br>";
+    }
+
+    function exercise4(){
+        include("includes/_ex4.html");   
+
+        if(isset($_POST['btn4'])){
+            $value = $_POST['txt4'];
+
+            if($value > 0){
+                echo "<div class='container'>";
+                //echo $value."<br>";
+                //echo $_POST['txt1'];
+                echo tabla_multiplicar($value);
+                echo "</div>";
+                echo "<br>";
+                echo "<br>";
+            }
+        }
+    }
+
+    function tabla_multiplicar($n) {
+        $tabla = "<table class='striped'>";
+        $tabla .= "<th>n</th>";
+        $tabla .= "<th>n^2</th>";
+        $tabla .= "<th>n^3</th>";
+        for ($i=1; $i <= $n; $i++) {
+            $tabla .= "<tr>";
+
+            for ($j=1; $j <= 1; $j++) {
+                $tabla .= "<th>".$i."</th>";
+                $tabla .= "<td>".$i*$i."</td>";
+                $tabla .= "<td>".$i*$i*$i."</td>";
+            }
+            $tabla .= "</tr>";
+        }
+        $tabla .= "</table>";
+        return $tabla;
+    }
+
+    function exercise5(){
+        include("includes/_ex5.html");   
+    }
+
+?>  
