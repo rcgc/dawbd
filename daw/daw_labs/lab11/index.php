3e557542034ca5006e40b84e66e608b8f598cc6c
rcgc <rcgc140298@gmail.com>
2019-02-28 22:14:36 -0600

    subir avance lab11

---
+++ daw/daw_labs/lab11/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+	require_once("util.php");
+	
+	encabezado();
+	imprimir_titulo("<div class='container'><h1 class='center-align'>Laboratorio 11</h1></div>");
+
+	set_tab_lab_number(11);
+
+	imprimir_subtitulo("<div class='container'><h3 class='left-align'>Ejercicios</h3></div><br>");
+	
+	imprimir_subtitulo("<div class='container'><h5 class='left-align'>Ejercicio W3C Schools</h5></div><br>");
+
+	form1();
+
+	action_form1();
+
+	imprimir_subtitulo("<div class='container'><h5 class='left-align'>Ejercicio personal</h5></div><br>");
+
+	imprimir_subtitulo("<br><div class='container'><h3 class='left-align'>Preguntas</h3></div><br>");
+
+	questions();
+
+	footer();	
+
+?>
---
+++ daw/daw_labs/lab11/util.php
@@ -0,0 +1,46 @@
+<?php
+
+	 function encabezado() {
+        include("includes/_navbar.html");
+    }
+
+     function footer() {
+        include("includes/_footer.html");
+    }
+
+    function questions(){
+        include("includes/_questions.html");   
+    }
+
+    function imprimir_titulo($titulo) {
+        echo "$titulo";
+    }
+
+    function imprimir_subtitulo($subtitulo){
+    	echo "$subtitulo";
+    }
+
+    function set_tab_lab_number($num){
+    	echo "<title>Lab".$num."</title>";
+    }
+
+    function form1(){
+        include("includes/_form1.html");
+        echo "<br>";
+    }
+
+    function action_form1(){
+        if(isset($_POST['btn_form1'])){
+                //$value = $_POST["txt1"];
+                echo "<div class='container'>";
+                echo "<div class='center-align'>";
+                echo "<strong>Nombre: </strong>".$_POST["name"]."<br>";
+                echo "<strong>E-mail: </strong>".$_POST["email"];
+                echo "</div>";
+                echo "</div>";
+                echo "<br>";
+                echo "<br>";
+        }
+    }
+
+?>  
