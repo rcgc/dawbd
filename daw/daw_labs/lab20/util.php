15b1c72d9dd0fcda1a3934ff1dfd544a6e9d1e9d
ROBERTO CARLOS GUZMAN CORTES <a01702388@itesm.mx>
2019-03-23 20:23:37 -0600

    prueba de no erroes lab 20

---
+++ daw/daw_labs/lab20/util.php
@@ -0,0 +1,14 @@
+<?php
+
+    function navbar(){
+        include("includes/_navbar.html");
+    }
+    
+    function questions(){
+        include("includes/_questions.html");
+    }
+    
+    function footer(){
+        include("includes/_footer.html");
+    }
+?>
